c2a53af598e7a5cead8a7bcb107b126fac0108cb
Tejbir Singh <singh093@csusm.edu>
2024-11-29 22:09:14 -0800

    Modify DirectionTest to force a test failure

--- src/test/java/org/psnbtech/DirectionTest.java
+++ src/test/java/org/psnbtech/DirectionTest.java
@@ -26,5 +26,13 @@ public class DirectionTest {
         assertEquals("West", Direction.West.toString());
     }
 
+    @Test
+public void testIsOpposite() {
+    Direction up = Direction.North;
+    Direction down = Direction.South;
+    assertFalse(up.isOpposite(down)); // This test currently passes
+}
+
+
     // You could add more tests here if the Direction enum had other methods
 }
